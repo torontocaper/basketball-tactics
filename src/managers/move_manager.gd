bb63a2c29d29d78817f0a78704745d3e22c4e89c
Adam <torontocaper@gmail.com>
2026-07-01 22:39:07 -0400

    add MoveManager scene

---
+++ src/managers/move_manager.gd
@@ -0,0 +1,78 @@
+#@tool
+@icon("uid://bfdiav77fq1x")
+class_name MoveManager
+extends Node
+## Documentation comments
+
+#signal
+enum NeighbourType {
+	ORTHOGONAL,
+	DIAGONAL
+}
+
+enum NeighbourDirection {
+	NORTH,
+	NORTHEAST,
+	EAST,
+	SOUTHEAST,
+	SOUTH,
+	SOUTHWEST,
+	WEST,
+	NORTHWEST
+}
+
+const MOVEMENT_COST_ORTHOGONAL: float = 1.0
+const MOVEMENT_COST_DIAGONAL: float= 1.5
+const TILE_SIZE: Vector2 = Vector2(48.0, 48.0)
+
+@export var court_cells: Array[Vector2i]:
+	set(value):
+		court_cells = value
+		print("MoveManager has a list of %s court cells" % court_cells.size())
+
+@export var selected_cell: Vector2i:
+	set(value):
+		selected_cell = value
+		print("MoveManager analysing cell %s" % selected_cell)
+		_move_selection_indicator(selected_cell)
+		var selected_cell_neighbours = get_neighbours(selected_cell)
+		print("This cell's neighbours: %s" % str(selected_cell_neighbours))
+
+@onready var selection_indicator: Polygon2D = %SelectionIndicator
+
+# OVERRIDES
+
+func _ready() -> void:
+	print("MoveManager ready")
+	_connect_signals()
+
+func _process(_delta: float) -> void:
+	pass
+
+func _physics_process(_delta: float) -> void:
+	pass
+
+# CORE
+func get_neighbours(cell: Vector2i) -> Dictionary:
+	var neighbours: Dictionary = {}
+	if cell not in court_cells:
+		print("Not a valid cell")
+	else:
+		neighbours["east"] = cell + Vector2i.RIGHT
+		neighbours["southeast"] = cell + (Vector2i.RIGHT + Vector2i.DOWN)
+		neighbours["south"] = cell + Vector2i.DOWN
+		neighbours["southwest"] = cell + (Vector2i.DOWN + Vector2i.LEFT)
+		neighbours["west"] = cell + Vector2i.LEFT
+		neighbours["northwest"] = cell + (Vector2i.LEFT + Vector2i.UP)
+		neighbours["north"] = cell + Vector2i.UP
+		neighbours["northeast"] = cell + (Vector2i.RIGHT + Vector2i.UP)
+	return neighbours
+
+# PRIVATE/HELPER
+func _connect_signals() -> void:
+	pass
+
+func _move_selection_indicator(new_position: Vector2) -> void:
+	selection_indicator.position = new_position * TILE_SIZE
+
+# RECEIVERS
--- src/world/court.gd
+++ src/world/court.gd
@@ -5,25 +5,16 @@ extends Area2D
 ## The surface a [Match] is played on.
 
 signal court_clicked
-#enum
-#const
-#@export var
-var cells: Array[Vector2i]
-var clicked_tile_coords:= Vector2i(-1, -1)
-#@onready var
-@onready var court_map: TileMapLayer = %CourtMap
 
-func _draw() -> void:
-	if not clicked_tile_coords == Vector2i(-1, -1):
-		var clicked_rect_size:= Vector2(court_map.tile_set.tile_size)
-		var clicked_rect_offset:= clicked_rect_size / 2.0
-		var clicked_rect_position:= court_map.map_to_local(clicked_tile_coords) - clicked_rect_offset
-		var clicked_rect:= Rect2(clicked_rect_position, clicked_rect_size)
-		draw_rect(clicked_rect, Color(Color.AQUA, 0.5), true)
+var clicked_tile_coords: Vector2i
+
+@onready var court_map: TileMapLayer = %CourtMap
+@onready var move_manager: MoveManager = %MoveManager
 
 func _ready() -> void:
+	print("Court ready")
 	_connect_signals()
-	cells = court_map.get_used_cells()
+	move_manager.court_cells = court_map.get_used_cells()
 
 func _process(_delta: float) -> void:
 	pass
@@ -43,5 +34,6 @@ func _on_input_event(_viewport:Node, event:InputEvent, _shape_idx: int):
 		print("Court clicked at %s" % get_local_mouse_position())
 		clicked_tile_coords = court_map.local_to_map(get_local_mouse_position())
 		print("Court clicked at tile %s" % clicked_tile_coords)
+		move_manager.selected_cell = clicked_tile_coords
 		court_clicked.emit(clicked_tile_coords)
-		queue_redraw()
+		#queue_redraw()
