e3833106a87c342804807c9d7d8d401924a37f33
Adam <torontocaper@gmail.com>
2026-07-27 21:50:06 -0400

    soften cell border

--- src/ui/cell_ui.gd
+++ src/ui/cell_ui.gd
@@ -10,12 +10,14 @@ var cell_cost : int:
 	set(value):
 		cell_cost = value
 		cell_cost_label.text = str(cell_cost)
+		cell_cost_label.show()
 		cell_alpha = float(cell_cost) * CELL_ALPHA_FACTOR
 
 var cell_alpha : float = 0.0:
 	set(value):
 		cell_alpha = value
 		cell_overlay.color.a = cell_alpha
+		cell_overlay.show()
 
 @onready var cell_cost_label: Label = $CellCost
 @onready var cell_overlay: ColorRect = $CellOverlay
